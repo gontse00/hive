af1240962ee72cd741cc62fbe20394055265c980
Gontse Kekane <kekanegontse@gmail.com>
2020-03-11 04:14:57 +0200

    Hive Data Defition Language

---
+++ partitions.sql
@@ -0,0 +1,22 @@
+CREATE DATABASE IF NOT EXISTS financials;
+USE financials;
+
+DROP TABLE IF EXISTS prices;
+CREATE TABLE IF NOT EXISTS prices (
+    ticker    STRING,
+    date_time    STRING,
+    open    FLOAT,
+    high    FLOAT,
+    low    FLOAT,
+    close    FLOAT,
+    volume    FLOAT,
+    ex_div    FLOAT,
+    split_ratio    FLOAT,
+    adj_open    FLOAT,
+    adj_high    FLOAT,
+    adj_low    FLOAT,
+    adj_close    FLOAT,
+    adj_volume    FLOAT)
+ROW FORMAT DELIMITED FIELDS TERMINATED BY ','
+TBLPROPERTIES ("skip.header.line.count"="1");
+DESCRIBE prices;
